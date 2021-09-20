016aad466eb48e98a685a924a067445f50a7383c
Phan Manh Quyet <phan_manhquyet@wohhup.com>
2021-09-21 00:21:32 +0800

    first comit

---
+++ src/util/util.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+import Jimp = require("jimp");
+import axios from "axios";
+
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  const imageBuffer = await axios({
+    method: "get",
+    url: inputURL,
+    responseType: "arraybuffer",
+  });
+
+  return new Promise((resolve, reject) => {
+    Jimp.read(imageBuffer.data)
+      .then((photo) => {
+        const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        photo
+          .resize(256, 256) // resize
+          .quality(60) // set JPEG quality
+          .greyscale() // set greyscale
+          .write(__dirname + outpath, (img) => {
+            resolve(__dirname + outpath);
+          });
+      })
+      .catch((err) => {
+        console.error(err);
+        reject("Could not read image.");
+      });
+  });
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
